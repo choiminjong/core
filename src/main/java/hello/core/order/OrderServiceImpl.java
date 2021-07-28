a4382496cc53204448ed2cde9ab120dbaf672133
alswhd20 <alswhd20@gmail.com>
2021-07-28 10:33:32 +0900

    관심사의 분리 인터페이스 의존성 클래스 분리

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,20 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
     private  final MemberRepository memberRepository = new MemoryMemberRepository();
-    private  final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private  final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    
+    //인터페이스에 난 해당 클래스 사용할거라고 선언 방식 new RateDiscountPolicy();
+    //private  final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
+    //인터페이스 선언 후 자동으로 클래스 선언될 수 있도록 설정이 필요
+    private  DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
