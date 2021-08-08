a58f12fb8ef782f03379f3744956ac2e5fea775f
gitlab_c <gitlab_c@nexon.co.kr>
2021-08-09 06:08:38 +0900

    appConfig.xml 설정방법

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -5,16 +5,19 @@ public class MemberServiceImpl implements MemberService{
     private final MemberRepository memberRepository;
 
     public MemberServiceImpl(MemberRepository memberRepository) {
+
         this.memberRepository = memberRepository;
     }
 
     @Override
     public void join(Member member) {
+
         memberRepository.save(member);
     }
 
     @Override
     public Member findMember(Long memberID) {
+
         return memberRepository.findById(memberID);
     }
 }
